4f3b057bb2fd01b66de2d5e8893d1d3f5e129ce1
andayanda <carmelaespinola@gmail.com>
2022-02-11 12:31:53 +0100

    bootstra

---
+++ M02/JS/js/primer.js
@@ -0,0 +1,18 @@
+d = new Date();
+document.getElementById("demo").innerHTML = d;
+console.log("data d'avui" + d);
+window.alert("Hola Mundo");
+//declaració de la variable
+var a;
+//també es pot declarar més d'una variable
+//a la mateixa línía
+var b, e, f, g;
+//fins i tot com s'ha vist anteriorment
+//amb vàries sentències en una línia
+var h; var i; var j;
+//també es pot assignar un valor des de la pantalla
+var c=prompt("Introdueix un valor");
+console.log(c);
+alert(c);
+var isGreater = 4 > 1;
+alert(isGreater); //True (el resultado de la comparación es "sí")

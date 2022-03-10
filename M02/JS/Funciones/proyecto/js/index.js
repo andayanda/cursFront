ba7b792cb608b9076966cece55316f1486d41fed
andayanda <carmelaespinola@gmail.com>
2022-03-10 12:23:32 +0100

    projecte

--- M02/JS/Funciones/proyecto/js/index.js
+++ M02/JS/Funciones/proyecto/js/index.js
@@ -1,6 +1,8 @@
-function buy(id){
-    console.table(products[id-1]);   
+
+function buy(id) {
+    console.table(products[id - 1]);
     for (let i = 0; i <= products.length; i++) {
-        cart.push(products[i]);        
-    }};
+        cart.push(products[i]);
+    }
     console.table(cart)
+};
